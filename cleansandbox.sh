2e741b3bd3b7f291d1edda257e32b102a34ba2b9
paul-kline <pauliankline@gmail.com>
2015-07-15 12:45:44 -0500

    renaming

--- cleansandbox.sh
+++ cleansandbox.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+
 pp=`locate bytestringJSON -n1`
+cd $pp
+./sandboxinit.sh
+cd -
+
 cabal sandbox delete
 cabal sandbox init
 cabal install $pp
-cabal install --dependencies-only
-cabal configure --enable-shared
-cabal install
-# cabal install
+
+cabal configure
+cabal build
+#cabal install
