8aa6ae5dd9cc4535f3c4170e7bde67ff5fc93048
root <ampetz@ku.edu>
2018-04-26 17:37:22 -0500

    separate run scripts for appraiser and attester

---
+++ demo/runApp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+te=$DEMO_PATH
+#ted=$DEPLOY_PATH
+#pr=$te"/provisioning"
+
+#make clean
+#(cd scripts ; ./restart_tpm.sh; ./build_all.sh; ./deploy_executables.sh)
+#make provision
+cd $te"/appraisal/"; ./AppMain
+#make run
+
--- demo/runAtt.sh
+++ demo/runAtt.sh
@@ -2,11 +2,11 @@
 
 te=$DEMO_PATH
 #ted=$DEPLOY_PATH
-pr=$te"/provisioning"
+#pr=$te"/provisioning"
 
 make clean
 (cd scripts ; ./restart_tpm.sh; ./build_all.sh; ./deploy_executables.sh)
 make provision
-cd $te"/appraisal/"; ./AppMain
+cd $te"/attestation/"; ./AttMain
 #make run
 
