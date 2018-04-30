9d9e719ab110173eae608b2495c3e9ff9678e7e2
Adam Petz <ampetz@ittc.ku.edu>
2018-04-30 11:16:14 -0500

    trying new app scripts

--- demo/scripts/deploy_executables.sh
+++ demo/scripts/deploy_executables.sh
@@ -13,10 +13,10 @@ echo $te"/attestation/"  | xargs -n 1 cp $swAtt"/AttMain"
 #echo $te/provisioning/ $ted/provisioning/ | xargs -n 1 cp $sw/ProvisioningMain
 echo $te"/provisioning/"  | xargs -n 1 cp $sw"/ProvisioningMain" 
 
-cp $swAtt/App1 $te/attestation/
-cp $swAtt/App1 $te/attestation/goodApp1
+cp $te/apps/happy $te/attestation/App1
+cp $te/apps/happy $te/attestation/goodApp1
 #cp $sw/App2 $te/attestation/
 #cp $sw/App2 $te/attestation/goodApp2
 
-cp $swAtt/BAD_App1 $te/attestation/badApp1
+cp $te/apps/unhappy $te/attestation/badApp1
 #cp $sw/BAD_App2 $te/attestation/badApp2
