7f0b5f7ad69cea98c55f7be6d1b7501460ccf07e
root <ampetz@ku.edu>
2018-05-01 14:45:15 -0500

    correct mac ip

--- demo/runAtt.sh
+++ demo/runAtt.sh
@@ -3,7 +3,7 @@
 te=$DEMO_PATH
 #ted=$DEPLOY_PATH
 #pr=$te"/provisioning"
-ipString="129.237.127.238"
+ipString="192.168.65.1"   #:56395" #"129.237.127.238"
 
 make clean
 (cd scripts ; ./restart_tpm.sh; ./build_all.sh; ./deploy_executables.sh)
