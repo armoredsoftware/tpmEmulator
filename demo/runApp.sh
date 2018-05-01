f1e3a9ca8f6c1da042e2f7dd4033e27e7908af0b
root <ampetz@ku.edu>
2018-04-30 23:03:42 -0500

    run on VM

--- demo/runApp.sh
+++ demo/runApp.sh
@@ -7,9 +7,16 @@ te=$DEMO_PATH
 #make clean
 #(cd scripts ; ./restart_tpm.sh; ./build_all.sh; ./deploy_executables.sh)
 #make provision
-cd $te"/appraisal/"; ./AppMain #make run
+
+for i in {1..4}
+do
+    cd $te"/appraisal/"; ./AppMain #make run
+    sleep 10s
+done
+
 #cd $te"/apps/"; ./unhappy
-cd $te"/attestation/"; ./App1
+#cd $te"/attestation/"; ./App1
+
 #rm "/home/adam/tpmEmulator/appraisal/appReq.txt"
 #rm "/home/adam/tpmEmulator/appraisal/attResp.txt"
 
