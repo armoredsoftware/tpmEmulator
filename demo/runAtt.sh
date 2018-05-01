cefb9c8f568d82a855e894c41d7813e0e098e8d6
root <ampetz@ku.edu>
2018-05-01 14:32:59 -0500

    runAtt

--- demo/runAtt.sh
+++ demo/runAtt.sh
@@ -3,10 +3,11 @@
 te=$DEMO_PATH
 #ted=$DEPLOY_PATH
 #pr=$te"/provisioning"
+ipString="129.237.127.238"
 
 make clean
 (cd scripts ; ./restart_tpm.sh; ./build_all.sh; ./deploy_executables.sh)
 make provision
-cd $te"/attestation/"; ./AttMain
+cd $te"/attestation/"; ./AttMain $ipString
 #make run
 
